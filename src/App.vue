2b936b40d9c2ce89e56e7817b25cd7be17dfece9
Lmcmaaser <lmcmaaser@gmail.com>
2020-01-27 05:10:57 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <PageOne />
+    <PageTwo />
+  </div>
+</template>
+
+<script>
+import PageOne from './components/PageOne.vue'
+import PageTwo from './components/PageTwo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PageOne,
+    PageTwo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
