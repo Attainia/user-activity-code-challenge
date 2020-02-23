5b777ff45c6d840b2f8ab35059cd77a901cdb420
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-23 17:22:42 -0600

    Fix missing semicolon

--- api/user/views.py
+++ api/user/views.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
 
-class UserViewSet(viewsets.ModelViewSet)
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
