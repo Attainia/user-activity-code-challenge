e87fe649c00e2b00ba10d50cf155084523c816af
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-23 16:41:46 -0600

    Create user model

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    last_login = models.DateField()
+    login_count = models.IntegerField()
+    project_count = models.IntegerField()
