c6c3fbc866a52b0bb62664efee85d5ed7a809386
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-24 10:05:11 -0600

    Add user table

--- app/src/components/ActiveUserDashboard.vue
+++ app/src/components/ActiveUserDashboard.vue
@@ -1,21 +1,47 @@
 <template>
   <div>
     <h1>Active User Dashboard</h1>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Last Login</th>
+        <th>Login Count</th>
+        <th>Project Count</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{user.id}}</td>
+        <td>{{user.username}}</td>
+        <td>{{user.last_login | formatDate}}</td>
+        <td>{{user.login_count}}</td>
+        <td>{{user.project_count}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import moment from 'moment'
+
 export default {
   name: 'ActiveUserDashboard',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      users: []
     }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:8000/users')
+      .then(response => (this.users = response.data))
+  },
+  filters: {
+    formatDate: (isoDateTime) => { return moment(isoDateTime).format('MM/DD/YYYY hh:mm') }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
