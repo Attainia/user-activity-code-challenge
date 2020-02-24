b7e126dda7ce68b723fba232426c6ec715b01412
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-24 10:06:06 -0600

    Remove unused styles

--- app/src/components/ActiveUserDashboard.vue
+++ app/src/components/ActiveUserDashboard.vue
@@ -43,18 +43,7 @@ export default {
 </script>
 
 <style scoped>
-h1, h2 {
+h1 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
