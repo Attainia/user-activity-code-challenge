ba0b4925c687b6319d97f36c7c23f233c521007b
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-24 10:23:32 -0600

    Add table styling

--- app/src/components/ActiveUserDashboard.vue
+++ app/src/components/ActiveUserDashboard.vue
@@ -46,4 +46,17 @@ export default {
 h1 {
   font-weight: normal;
 }
+
+table {
+  margin-left: auto;
+  margin-right: auto;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+
+td, th {
+  padding: 5px 15px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
 </style>
