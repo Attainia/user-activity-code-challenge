e5ca413b79e808c5c55edf73b2eb79db9be64a8b
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-24 15:41:24 -0600

    Add activity filtering

--- api/user/views.py
+++ api/user/views.py
@@ -4,5 +4,19 @@ from .serializers import UserSerializer
 from .models import User
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all().order_by('id')
+        activity_filter = self.request.query_params.get('active', None)
+
+        if activity_filter is not None:
+            queryset = self.__filter_activity(activity_filter, queryset)
+
+        return queryset
+
+    def __filter_activity(self, activity_filter, queryset):
+        if activity_filter == 'true':
+            return queryset.filter(login_count__gt=0)
+        else:
+            return queryset.filter(login_count=0)
