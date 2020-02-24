4b0ecd50b4d096b0a14f02677a70218a191c4e25
Patrick Fierro <Patrick.John.Fierro@gmail.com>
2020-02-24 09:01:06 -0600

    Remove unused default data

--- app/src/components/Directory.vue
+++ app/src/components/Directory.vue
@@ -21,14 +21,11 @@
 export default {
   name: 'Directory',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    return {}
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
