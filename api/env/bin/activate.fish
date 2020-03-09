b1af767fec6b725c02c4a9851bf9da8515e1b074
nicksimmons53 <nicksimmons53@gmail.com>
2020-03-09 08:38:07 -0500

    Removed node_modules from repo

--- api/env/bin/activate.fish
+++ api/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nick/Projects/user-activity-code-challenge/api/env"
+set -gx VIRTUAL_ENV "/Users/nick/Test/user-activity-code-challenge/api/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
