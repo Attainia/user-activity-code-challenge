fb555e854fdd651f33b3f6c25c80070c0cdcf24a
Daniel Smith <djsmithfreelance@gmail.com>
2020-12-09 17:58:07 -0600

    Created users api and entered user data to db

--- testapp/usersapi/models.py
+++ testapp/usersapi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=30)
+    last_login = models.DateTimeField()
+    project_count = models.IntegerField()
+    login_count = models.IntegerField()
--- testapp/usersapi/views.py
+++ testapp/usersapi/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
